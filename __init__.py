dc7f9da29cd362275547f3d16e86c3a34b9ce93d
LÉDÉE François <5116562-fledee@users.noreply.gitlab.com>
2021-11-08 15:35:00 -0800

    Structural Change

--- __init__.py
+++ __init__.py
@@ -1,8 +1,1 @@
-### Initialization file for the module computing the impacts of the electric grid.
 
-import os
-import sys
-
-# Allows to refer to sub-modules within the module
-CURRENT_DIR = os.path.dirname( os.path.abspath(r"{}".format(__file__)) )
-sys.path.append(CURRENT_DIR)
---
+++ setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+import os
+
+dir_path = os.path.dirname(os.path.realpath(__file__))
+
+with open(dir_path+"/README.md", "r") as fh:
+    long_description = fh.read()
+with open(dir_path+"/requirements.txt") as f:
+    requirements = f.read().splitlines()
+
+
+setup(
+    name="dynamical",
+    version="0.1.0",
+    description="A library for dynamic LCA of european electricity.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author="Francois Ledee",
+    author_email="ledee.francois@gmail.com",
+    url="https://gitlab.com/fledee/ecodyn/",
+    packages=["dynamical"],
+    include_package_data=True,
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: Apache License, Version 2.0",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=requirements,
+)
