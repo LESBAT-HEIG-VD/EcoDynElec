1c83892672ba3d97b026c5d627fe1042d037a9a1
LEDEE Francois <5116562-fledee@users.noreply.gitlab.com>
2022-08-05 18:55:44 +0000

    Modify init files for correct imports

--- dynamical/__init__.py
+++ dynamical/__init__.py
@@ -1,8 +1,10 @@
 ### Initialization file for the module computing the impacts of the electric grid.
+__all__ = ['parameter','load_data','checking','easy_use','impacts','parameters','residual','saving','tracking']
+from dynamical import *
 
-import os
-import sys
+# import os
+# import sys
 
-# Allows to refer to sub-modules within the module
-CURRENT_DIR = os.path.dirname( os.path.abspath(r"{}".format(__file__)) )
-sys.path.append(CURRENT_DIR)
+# # Allows to refer to sub-modules within the module
+# CURRENT_DIR = os.path.dirname( os.path.abspath(r"{}".format(__file__)) )
+# sys.path.append(CURRENT_DIR)
--- dynamical/load_data/__init__.py
+++ dynamical/load_data/__init__.py
@@ -1,4 +1,6 @@
-from . import auxiliary
-from . import generation_exchanges
-from . import impacts
-from . import raw_entsoe
+__all__ = ['auxiliary','download_raw','generation_exchanges','impacts','raw_entsoe']
+from dynamical.load_data import *
+# from . import auxiliary
+# from . import generation_exchanges
+# from . import impacts
+# from . import raw_entsoe
