24bc1346f3aa3a8ce6879c2bc17f8f6c6d78b287
LEDEE Francois <5116562-fledee@users.noreply.gitlab.com>
2023-02-25 04:00:05 +0000

    update with adding data

--- ecodynelec/__init__.py
+++ ecodynelec/__init__.py
@@ -1,4 +1,5 @@
 ### Initialization file for the module computing the impacts of the electric grid.
 ### ORDER IN THE LIST DOES MATTER
+# Updating left outside on purpose -> needs to import the whole way manually
 __all__ = ['checking','preprocessing','impacts','parameter','saving','tracking','pipelines']
 from ecodynelec import *
