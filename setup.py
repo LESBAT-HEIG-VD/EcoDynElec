10321f9d56e245c740eb3cebc3ff3dd174ce5c77
aymeric.bourdy <aymeric.bourdy@heig-vd.ch>
2023-12-16 17:58:40 +0100

    Fix setup.py

--- setup.py
+++ setup.py
@@ -3,20 +3,23 @@ import os
 
 dir_path = os.path.dirname(os.path.realpath(__file__))
 
-with open(dir_path+"/README.md", "r") as fh:
+with open(os.path.join(dir_path, "README.md"), "r", encoding="utf-8") as fh:
     long_description = fh.read()
-with open(dir_path+"/requirements.txt") as f:
-    requirements = f.read().splitlines()
+if os.path.exists(os.path.join(dir_path, "requirements.txt")):
+    with open(os.path.join(dir_path, "requirements.txt")) as f:
+        requirements = f.read().splitlines()
+else:
+    requirements = ['numpy', 'pandas', 'openpyxl', 'xlrd', 'odfpy', 'paramiko', 'matplotlib', 'tabula-py']
 
 
 setup(
     name="ecodynelec",
-    version="0.1.0",
+    version="0.2.0",
     description="A library for dynamic LCA of european electricity.",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    author="Francois Ledee",
-    author_email="ledee.francois@gmail.com",
+    author="Francois Ledee, Aymeric Bourdy",
+    author_email="ledee.francois@gmail.com, aymericb5@gmail.com",
     url="https://gitlab.com/fledee/ecodynelec/",
     packages=find_packages(".", exclude=['test']),
     include_package_data=True,
